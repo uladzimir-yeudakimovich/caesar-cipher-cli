889a020f2af1e832306e10ebb130ce37a1555beb
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-03-29 01:53:55 +0300

    improved validation

--- caesar-cipher-cli/validation.js
+++ caesar-cipher-cli/validation.js
@@ -2,18 +2,24 @@ const { program } = require('commander');
 
 program
   .requiredOption('-s, --shift <number>', 'a shift')
-  .requiredOption('-a, --action <type>,', 'an action encode/decode')
+  .requiredOption('-a, --action <type>', 'an action encode/decode')
   .option('-i, --input <filename>', 'an input file')
   .option('-o, --output <filename>', 'an output file')
   .parse(process.argv);
 
-if (
-  !['encode', 'decode'].includes(program.action) ||
-  isNaN(program.shift) ||
-  !Number.isInteger(+program.shift)
-) {
+if (!['encode', 'decode'].includes(program.action)) {
   process.stderr.write(
-    'Action and shift are required. Please, enter valid data and try again!'
+    "Action is required and should be have name 'encode' or 'decode'. Please stop the server, enter the correct data and try again!"
+  );
+  process.exitCode = 1;
+} else if (isNaN(program.shift)) {
+  process.stderr.write(
+    'Shift should be number. Please stop the server, enter the correct data and try again!'
+  );
+  process.exitCode = 1;
+} else if (!Number.isInteger(+program.shift)) {
+  process.stderr.write(
+    'Shift should be integer. Please stop the server, enter the correct data and try again!'
   );
   process.exitCode = 1;
 }
